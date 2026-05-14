331a13587e459b9274e678f17edaae7384d547ad
Castelblanco <ejcastelg@gmail.com>
2026-05-13 23:26:24 -0500

    fix: is init current route for the aobrt manager, without this, the code generate an error

--- src/components/layouts/LayoutMain.vue
+++ src/components/layouts/LayoutMain.vue
@@ -2,6 +2,7 @@
 import { appRouter } from '@/router';
 import type { ROUTES } from '@constants/routes';
 import { abortManager } from '@storages/stores/abort_controllers';
+abortManager.setCurrentRoute(appRouter.currentRoute.value.path as ROUTES);
 appRouter.beforeResolve((_, from) => {
     abortManager.setCurrentRoute(from.path as ROUTES);
 });
