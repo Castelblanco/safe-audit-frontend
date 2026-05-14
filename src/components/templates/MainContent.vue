cc99e195b253b3a4406ab6bc3e9e35cd9c550729
Castelblanco <ejcastelg@gmail.com>
2026-05-13 23:28:59 -0500

    feat: is implemented new template main content, since there are several layouts, all the layouts will used this component

---
+++ src/components/templates/MainContent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { QSeparator } from 'quasar';
+
+export type TMainContentProps = {
+    title?: string;
+};
+const { title } = defineProps<TMainContentProps>();
+</script>
+
+<template>
+    <main class="tw:p-3 tw:w-full">
+        <div v-if="title">
+            <h4>{{ title }}</h4>
+            <QSeparator />
+        </div>
+        <slot />
+    </main>
+</template>
