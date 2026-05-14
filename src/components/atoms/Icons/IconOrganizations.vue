3c1d4e6734420b6f61c97ae675c386930a953dfe
Castelblanco <ejcastelg@gmail.com>
2026-05-13 23:30:50 -0500

    feat: is added new icons

---
+++ src/components/atoms/Icons/IconOrganizations.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Building2 } from '@lucide/vue';
+import type { TIcon } from './type';
+const props = defineProps<TIcon>();
+</script>
+
+<template>
+    <Building2 v-bind="props" />
+</template>
