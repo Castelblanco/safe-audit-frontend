22e535e0d3800d7133b1561a88edc47e9a3d1d66
Castelblanco <ejcastelg@gmail.com>
2026-05-14 23:41:59 -0500

    refactor: is change interaction with the session store, now this is moved to layout app

--- src/components/pages/auth/Login.vue
+++ src/components/pages/auth/Login.vue
@@ -4,10 +4,11 @@ import InputText from '@/components/atoms/Inputs/InputText.vue';
 import H1 from '@/components/atoms/Typography/H1.vue';
 import { ROUTES } from '@constants/routes';
 import { handleResponse } from '@helpers/handle_response';
+import InputPassword from '@molecules/Inputs/InputPassword.vue';
 import { servicesSession } from '@services/session';
 import { useSessionStore } from '@storages/stores/session';
 import { snackbarStore } from '@storages/stores/snackbar';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { useRouter } from 'vue-router';
 const sessionStore = useSessionStore();
 const router = useRouter();
@@ -16,15 +17,26 @@ const email = ref('');
 const password = ref('');
 const loading = ref(false);
 
+watch(
+    () => sessionStore.value,
+    () => {
+        if (sessionStore.value !== undefined) {
+            router.replace(ROUTES.DASHBOARD);
+        }
+    },
+    {
+        immediate: true,
+    },
+);
+
 const handleLogin = async () => {
     loading.value = true;
     const result = await servicesSession.login(email.value, password.value);
     handleResponse(result, {
         success(data) {
             sessionStore.value = data.item;
-            router.replace(ROUTES.DASHBOARD);
         },
-        error(error) {
+        error() {
             snackbarStore.success('Error');
         },
     });
@@ -47,13 +59,11 @@ const handleLogin = async () => {
                         class="tw:w-full"
                         label="correo"
                         name="email"
-                        au
                     />
-                    <InputText
+                    <InputPassword
                         v-model="password"
                         class="tw:w-full"
                         label="contraseña"
-                        type="password"
                         name="password"
                     />
                     <ButtonPrimary type="submit" :loading="loading">
