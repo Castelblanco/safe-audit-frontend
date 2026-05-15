612582284cceb4a45a630fb96c561297b4ad6ace
Castelblanco <ejcastelg@gmail.com>
2026-05-14 23:37:33 -0500

    feat: is added new icons

---
+++ src/components/atoms/Icons/IconNotVisibility.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { EyeOff } from '@lucide/vue';
+import type { TIcon } from './type';
+const props = defineProps<TIcon>();
+</script>
+
+<template>
+    <EyeOff v-bind="props" />
+</template>
---
+++ src/components/atoms/Icons/IconVisibility.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Eye } from '@lucide/vue';
+import type { TIcon } from './type';
+const props = defineProps<TIcon>();
+</script>
+
+<template>
+    <Eye v-bind="props" />
+</template>
