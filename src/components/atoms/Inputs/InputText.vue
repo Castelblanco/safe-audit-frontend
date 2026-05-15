95d65201067060dd934849bec3b6c210cdc7b019
Castelblanco <ejcastelg@gmail.com>
2026-05-14 23:39:47 -0500

    fix: is correct the slots for the input, now assing correctly the slots

--- src/components/atoms/Inputs/InputText.vue
+++ src/components/atoms/Inputs/InputText.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 import { QInput } from 'quasar';
 
-export type TInputProps = {
+export type TInputTextProps = {
     label?: string;
     class?: string;
     type?: 'text' | 'password';
     name?: string;
 };
 const model = defineModel<string | undefined | null>();
-const { label, ...props } = defineProps<TInputProps>();
+const { label, ...props } = defineProps<TInputTextProps>();
 </script>
 
 <template>
-    <q-input outlined autocomplete="off" v-model="model" :label="label" v-bind="props" />
+    <QInput outlined autocomplete="off" v-model="model" :label="label" v-bind="props">
+        <template v-for="(_, slotName) in $slots" :key="slotName" #[slotName]>
+            <slot :name="slotName" />
+        </template>
+    </QInput>
 </template>
